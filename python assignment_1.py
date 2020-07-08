d53d4045285fe2c74fa9feea8c5965d6c8ded282
Enes iştem <enestm93@gmail.com>
2020-07-08 12:06:22 +0300

    if assignment 1 ödevi

---
+++ python assignment_1.py
@@ -0,0 +1,6 @@
+name = "enes"
+password = input("please enter your name : ")
+if password == name:
+    print("Hello Joseph! the password is : w@12 ")
+else:
+    print("Hello, Amina! See you later")    
